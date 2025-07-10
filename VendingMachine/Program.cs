9e72ed6444bf28aa3761827d2a005649e78e8fdd
tcs-r-terashima <r-terashima@tcs-s.co.jp>
2025-07-10 13:35:25 +0900

    プロジェクト ファイルを追加します。

---
+++ VendingMachine/Program.cs
@@ -0,0 +1,29 @@
+﻿
+class VendingMachine
+{
+    private List<String> products = new List<String>();
+    private int insertedMoney = 0;
+    private int totalSales = 0;
+
+    public VendingMachine() {
+
+        products.Add("ファンタ");
+        products.Add("お茶");
+        products.Add("コーヒー");
+    }
+
+
+    public InsertedMoney(int money)
+    {
+        insertedMoney += money;
+        Console.WriteLine($"{money}円を投入しました。現在の投入額: {insertedMoney}円");
+
+
+    }
+
+    public void ShowProducts()
+    {
+
+
+    }
+}
