97da588bbba53c2557af0a94babcb4a8f8ab8268
tcs-r-terashima <r-terashima@tcs-s.co.jp>
2025-07-10 14:31:21 +0900

    Product修正

--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -1,15 +1,32 @@
 ﻿
+using System.Diagnostics;
+using System.Xml.Linq;
+
+class Product
+{
+    public string Name { get; set; }
+    public int Stock { get; set; }
+    public int Price { get; set; }
+
+    public Product(string name, int stock, int price)
+    {
+        Name = name;
+        Stock = stock;
+        Price = price;
+    }
+}
 class VendingMachine
 {
-    private List<String> products = new List<String>();
+
+    private List<Product> products = new List<Product>();
     private int insertedMoney = 0;
     private int totalSales = 0;
 
     public VendingMachine() {
 
-        products.Add("ファンタ");
-        products.Add("お茶");
-        products.Add("コーヒー");
+        products.Add(new Product("ファンタ",5,150));
+        products.Add(new Product("お茶", 5, 150));
+        products.Add(new Product("コーヒー", 5, 150));
     }
 
 
